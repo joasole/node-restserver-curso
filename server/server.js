60fc22535ab7681aff401cff8e434fe488d3db67
joanita <joa.pedernera@gmail.com>
2020-07-28 19:50:30 -0300

    primir comit sola

---
+++ server/server.js
@@ -0,0 +1,67 @@
+require('./config/config');
+
+const express = require('express');
+const app = express();
+
+const bodyParser = require('body-parser')
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+
+
+// OBTENER
+app.get('/usuario', function(req, res) {
+    res.json('GET USUARIO -OBTENER-');
+});
+
+//CREAR REGISTROS
+app.post('/usuario', function(req, res) {
+
+    let body = req.body;
+
+    if (body.nombre === undefined) {
+
+        res.status(400).json({
+            ok: false,
+            mensaje: 'El nombre es necesario'
+        });
+
+    } else {
+        res.json({
+            persona: body
+        });
+    }
+
+});
+
+//ACTUALIZAR REGISTROS
+app.patch('/usuario', function(req, res) {
+    res.json(' PATCH USUARIO - ACTUALIZAR REGISTROS');
+});
+
+
+//ACTULIZAR DATA
+app.put('/usuario/:id', function(req, res) {
+
+
+    let id = req.params.id;
+
+    res.json({
+        id
+    });
+});
+
+
+//BORARA - PERO QUEDA REGISTRO
+app.delete('/usuario', function(req, res) {
+    res.json(' DELETE USUARIO -BORRARRRR-');
+});
+
+
+app.listen(process.env.PORT, () => {
+    console.log('Escuchando en el puerto:', process.env.PORT);
+});
